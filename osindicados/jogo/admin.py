986778ee1cf652c8e7cbf4137cd9b08e54d474d4
carlaguill <carlaguill@72fd24ee-5b7e-17c4-4885-9f4f3847c48f>
2010-07-09 15:44:37 +0000

    - alteração de todos os tabs por espaço de tamanho 4

--- osindicados/jogo/admin.py
+++ osindicados/jogo/admin.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.contrib import admin 
+from django.contrib import admin
 from osindicados.jogo.models import Pergunta
 from osindicados.jogo.models import Tema
 
--- osindicados/jogo/models.py
+++ osindicados/jogo/models.py
@@ -2,20 +2,20 @@
 from django.db import models
 
 class Tema(models.Model):
-	nome = models.CharField(max_length=30)
-	nomeTrofeu = models.CharField(max_length=30)
-	imagemTrofeu = models.CharField(max_length=100)
-	def __unicode__(self):
-		return self.nome
-    
+    nome = models.CharField(max_length=30)
+    nomeTrofeu = models.CharField(max_length=30)
+    imagemTrofeu = models.CharField(max_length=100)
+    def __unicode__(self):
+        return self.nome
+
 class Pergunta(models.Model):
-	enunciado = models.CharField(max_length=1000)
-	altCorreta = models.CharField(max_length=200)
-	altIncorreta1 = models.CharField(max_length=200)
-	altIncorreta2 = models.CharField(max_length=200)
-	altIncorreta3 = models.CharField(max_length=200)
-	altIncorreta4 = models.CharField(max_length=200)
-	dificuldade = models.IntegerField()
-	idAssunto = models.ForeignKey(Tema)
-	def __unicode__(self):
-		return self.enunciado
+    enunciado = models.CharField(max_length=1000)
+    altCorreta = models.CharField(max_length=200)
+    altIncorreta1 = models.CharField(max_length=200)
+    altIncorreta2 = models.CharField(max_length=200)
+    altIncorreta3 = models.CharField(max_length=200)
+    altIncorreta4 = models.CharField(max_length=200)
+    dificuldade = models.IntegerField()
+    idAssunto = models.ForeignKey(Tema)
+    def __unicode__(self):
+        return self.enunciado
