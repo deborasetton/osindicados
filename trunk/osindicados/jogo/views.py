270070c5589ab13d8010d925e1059923412fcf4b
carlaguill <carlaguill@72fd24ee-5b7e-17c4-4885-9f4f3847c48f>
2010-07-11 21:34:09 +0000

    - Inclusão de 30 perguntas de TV no banco - Mudança na index.html (página de teste) para mostrar o número de perguntas cadastradas em cada Tema - Mudança no models.py para exibir na tela de admin combo de dificuldades

--- trunk/osindicados/jogo/views.py
+++ trunk/osindicados/jogo/views.py
@@ -5,5 +5,9 @@ from django.shortcuts import render_to_response
 def index(request):
     temas = Tema.objects.all()
     perguntas = Pergunta.objects.all()
-    return render_to_response('jogo/index.html', {'temas': temas, 'perguntas': perguntas})
+
+    # calcula quantas perguntas foram cadastradas em cada tema
+    list_tema_qntd = [(tema, len(Pergunta.objects.filter(idAssunto__exact=tema.id))) for tema in temas]
+
+    return render_to_response('jogo/index.html', {'list_tema_qntd': list_tema_qntd, 'perguntas': perguntas})
 
