964262c963e924016ec704d80e88db4edf6f6c56
carlaguill <carlaguill@72fd24ee-5b7e-17c4-4885-9f4f3847c48f>
2010-07-11 21:34:09 +0000

    - Inclusão de 30 perguntas de TV no banco - Mudança na index.html (página de teste) para mostrar o número de perguntas cadastradas em cada Tema - Mudança no models.py para exibir na tela de admin combo de dificuldades

--- osindicados/jogo/models.py
+++ osindicados/jogo/models.py
@@ -9,13 +9,19 @@ class Tema(models.Model):
         return self.nome
 
 class Pergunta(models.Model):
+    DIFICULDADES = (
+                    (1, 'fácil'),
+                    (2, 'médio'),
+                    (3, 'difícil'),
+                    (4, 'desafio')
+                    )
     enunciado = models.CharField(max_length=1000)
     altCorreta = models.CharField(max_length=200)
     altIncorreta1 = models.CharField(max_length=200)
     altIncorreta2 = models.CharField(max_length=200)
     altIncorreta3 = models.CharField(max_length=200)
     altIncorreta4 = models.CharField(max_length=200)
-    dificuldade = models.IntegerField()
+    dificuldade = models.IntegerField(choices=DIFICULDADES)
     idAssunto = models.ForeignKey(Tema)
     def __unicode__(self):
         return self.enunciado
