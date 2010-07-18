d2d8c5fbc4ead2cade0a0789634ee1dba1586884
desetton <desetton@72fd24ee-5b7e-17c4-4885-9f4f3847c48f>
2010-07-18 23:13:26 +0000

    *Configuração para usar JS, CSS e imagens Para fazer com que o servidor sirva algum tipo de arquivo que não um template (javascript, css, imagens), colocar o conteúdo dentro da pasta media e a url deve começar com "/osindicadosmedia/", por exemplo: /osindicadosmedia/img/teste.jpeg

--- trunk/osindicados/urls.py
+++ trunk/osindicados/urls.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls.defaults import *
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -8,6 +9,7 @@ admin.autodiscover()
 urlpatterns = patterns('',
                        (r'^jogo/', include('osindicados.jogo.urls')),
                        (r'^admin/', include(admin.site.urls)),
+                       (r'^osindicadosmedia/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
                        (r'^$', include(admin.site.urls)),
                        )
 
